f46658ecf49ca66bfbafd532204dcd810d0469af
gammabray <brayj456@gmail.com>
2016-11-23 21:59:12 +0000

    Added copy constructor.

--- mario cplus/DisplayObject.cpp
+++ mario cplus/DisplayObject.cpp
@@ -7,9 +7,18 @@ Game::DisplayObject::DisplayObject(const Vector2f& startPosition, const Vector2f
 	scaleFactor = Vector2f(1, 1);
 	texture = std::make_unique<sf::Texture>();
 	sprite = std::make_unique <sf::Sprite>();
-	sprite->setTexture(*texture);
 	sprite->setPosition(position);
 	collisionBox;
+}
+
+Game::DisplayObject::DisplayObject(const DisplayObject & copy)
+{
+	this->position = copy.position;
+	this->size = copy.size;
+	this->scaleFactor = copy.scaleFactor;
+	this->texture = std::make_unique<sf::Texture>(*copy.texture);
+	this->sprite = std::make_unique<sf::Sprite>(*copy.sprite);
+	this->collisionBox = copy.collisionBox;
 }
 
 Game::DisplayObject::~DisplayObject()
--- mario cplus/DisplayObject.hpp
+++ mario cplus/DisplayObject.hpp
@@ -3,16 +3,22 @@
 
 #ifndef DISPLAYOBJECT_HPP
 #define DISPLAYOBJECT_HPP
-
-
 #include <SFML\Graphics.hpp>
 #include <memory>
 
 using sf::Vector2f;
 
-//Base class for all drawable objects in game
 namespace Game {
 	class DisplayObject {
+		///<summary>///
+		/// Class: DisplayObject (Abstract)
+		/// 
+		///	The base class for all 'entities' in the game. This includes enemies and the character.
+		///	It is not a base class for the Level and Background tiles as they are essentially containers for Level and Background Tiles.
+		///
+		///
+		///
+		///</summary>
 
 	protected:
 		Vector2f position;
@@ -29,9 +35,10 @@ namespace Game {
 		void setSize(const Vector2f& value) { size = value; }
 		sf::Sprite getSprite() { return *sprite; }
 		const sf::FloatRect& getCollisionBox() { return collisionBox; }
-		virtual void Draw(sf::RenderTarget& target, const sf::RenderStates& states = sf::RenderStates::Default) = 0;
-		DisplayObject(const Vector2f& startPos, const Vector2f& startSize);
-		virtual ~DisplayObject();
+		DisplayObject(const DisplayObject& copy);
+		virtual void Draw(sf::RenderTarget& target, const sf::RenderStates& states = sf::RenderStates::Default) = 0;//pure virtual function : implementation left to derived classes
+		DisplayObject(const Vector2f& startPos, const Vector2f &startSize);
+		virtual ~DisplayObject();//virtual destructor so called first
 
 
 
