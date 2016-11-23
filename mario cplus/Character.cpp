9a5820328d87ee3670d2a596668bcce8882282e5
gammabray <brayj456@gmail.com>
2016-11-23 21:58:26 +0000

    Changed what is used to handle input and (keypressed & keyreleased -> iskeypressed).  Fixed bug where jumping would move the player to the right regardless of where they were facing before

--- mario cplus/Character.cpp
+++ mario cplus/Character.cpp
@@ -5,19 +5,19 @@
 Game::Character::Character(const Vector2f& startPos, const Vector2f& startSize) 
 	: AnimatedObject(startPos,startSize,6)
 	,IsJumping(false), IsWalking(false),StartSpeed(0.2f,1.f)
-//remember to add no of sprites later
+
 {
 	animationClock.restart();
 	travelling = direction::STATIONARY;
 	acceleration = Vector2f(0.f, 0.08f);
 	if (!texture->loadFromFile("Images\\marioSpriteSheet.gif")) {
-		texture.release();
+		texture.release();//delete texture
 		std::cout << "Failed to load Texture for Character..." << std::endl;
 	}
 	sprite->setTexture(*texture);
-	sprite->setOrigin(startSize.x / 2, startSize.y / 2);
+	sprite->setOrigin(startSize.x / 2, startSize.y / 2);//middle of character
 	addSprites();
-	sprite->setTextureRect(SpriteStates[0]);
+	sprite->setTextureRect(SpriteStates[0]);//start standing
 	sprite->setPosition(position);
 	sprite->setScale(scaleFactor);
 	IsVisible = true;
@@ -26,6 +26,7 @@ Game::Character::Character(const Vector2f& startPos, const Vector2f& startSize)
 
 
 void Game::Character::addSprites()
+//Adds the location data of each sprite
 {
 	
 	SpriteStates.push_back(sf::IntRect(3,0,13,17));
@@ -34,59 +35,79 @@ void Game::Character::addSprites()
 	SpriteStates.push_back(sf::IntRect(94,0,17,17));
 	SpriteStates.push_back(sf::IntRect(SpriteStates[2]));
 	SpriteStates.push_back(sf::IntRect(155,0,17,17));
+	SpriteStates.push_back(sf::IntRect(187, 17, 15, 17));
 }
-void Game::Character::update(sf::View& v, sf::Keyboard::Key k)
+void Game::Character::update(sf::View& v)
+//updates the position of the sprite on the screen
 {
 	changeSprite();
-	float delta = static_cast<float>( speedClock.restart().asMilliseconds());
+	float delta = static_cast<float>( speedClock.restart().asMilliseconds());//time since last frame
+	collisionBox = sprite->getGlobalBounds();//set collision box
 	if (IsJumping) {
 		this->move(delta, v);
 	}
-	if (k == sf::Keyboard::Up) {
-		travelling = direction::UP;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
 		this->jump();
+		travelling = direction::UP;
+		
 
 	}
-	if (k == sf::Keyboard::Left)
-	{
-		sprite->setScale(-1, 1);
-		travelling = direction::LEFT;
-		velocity.x = StartSpeed.x;
-		this->move(delta, v);
+	else {
+		if (IsJumping) return;
+		
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
+			sprite->setScale(-1, 1);
+			travelling = direction::LEFT;
+			velocity.x = StartSpeed.x;
+			this->move(delta, v);
+			return;
 
-	}
-	if (k == sf::Keyboard::Right)
-	{
-		sprite->setScale(1, 1);
-		velocity.x = StartSpeed.x;
-		travelling = direction::RIGHT;
-		this->move(delta, v);
-
-	}
-
-	
-	if(k == sf::Keyboard::Down){
-		travelling = direction::DOWN;
-	}
-	if (k == sf::Keyboard::Unknown) {
-		if (!IsJumping) {
-			velocity.x = 0;
-			travelling = direction::STATIONARY;
 		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		{
+			sprite->setScale(1, 1);
+			velocity.x = StartSpeed.x;
+			travelling = direction::RIGHT;
+			this->move(delta, v);
+			return;
+
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+			travelling = direction::DOWN;
+			return;
+		}
+		travelling = direction::STATIONARY;
 	}
 }
 void Game::Character::jump()
+//checks if the player is already jumping.
+//if not, store the velocity before jumping
+//and change the state of the object to jumping
 {
 	if (!IsJumping) {
+		switch (travelling) {
+		case direction::LEFT:
+			VelocityBeforeJumping.x = -velocity.x;
+			break;
+		case direction::RIGHT:
+			VelocityBeforeJumping.x = velocity.x;
+			break;
+		default:
+			VelocityBeforeJumping.x = 0;
+			break;
+		}
 		IsJumping = true;
 		IsWalking = false;
-		VelocityBeforeJumping = velocity;
+		
 		velocity.y = StartSpeed.y;
 		
 	}
 	
 }
 void Game::Character::move(float delta, sf::View& v)
+//change the position of the player depending on whether
+//they are jumping or not.
 {
 	
 	switch (travelling) {
@@ -95,7 +116,7 @@ void Game::Character::move(float delta, sf::View& v)
 			return;
 		}
 		IsWalking = true;
-		//v.move(delta * velocity.x, 0.f);
+		v.move(delta * -velocity.x, 0.f);
 		this->sprite->move(delta * -velocity.x, 0.f);
 		this->position += sf::Vector2f(delta * -velocity.x, 0.f);
 		break;
@@ -104,7 +125,7 @@ void Game::Character::move(float delta, sf::View& v)
 			return;
 		}
 		IsWalking = true;
-		//v.move(delta * -velocity.x, 0.f);
+		v.move(delta * velocity.x, 0.f);
 		this->sprite->move(delta * velocity.x, 0.f);
 		this->position += sf::Vector2f(delta * velocity.x, 0.f);
 		break;
@@ -115,29 +136,27 @@ void Game::Character::move(float delta, sf::View& v)
 				this->sprite->setPosition(sf::Vector2f(this->position.x,600));
 				velocity = VelocityBeforeJumping;
 				IsJumping = false;
-				IsWalking = true;
+				IsWalking = false;
 				return;
 			}
 			velocity.y -= acceleration.y;
-			//v.move(delta * VelocityBeforeJumping.x, 0);
+			v.move(delta * VelocityBeforeJumping.x, 0);
 			this->sprite->move(delta * VelocityBeforeJumping.x, delta * -velocity.y);
+			
 			this->position += sf::Vector2f(delta * VelocityBeforeJumping.x, delta * -velocity.y);
 		}
 		break;
 	case direction::STATIONARY:
 		velocity.x = 0;
 		IsWalking = false;
-
+		currSprite = 0;
 		break;
 	
 		
 	}
 }
 
-std::ostream& operator<<(std::ostream& o, const Vector2f& vec) {
-	o << "x: " << vec.x << " y: " << vec.y;
-	return o;
-}
+
 
 void Game::Character::DisplayInfo()
 
@@ -153,16 +172,15 @@ void Game::Character::Draw(sf::RenderTarget & target, const sf::RenderStates & s
 
 void Game::Character::changeSprite(int changeTo)
 {
-	if (this->travelling == direction::STATIONARY) {
-		currSprite = 0;
-		animationClock.restart();
-	}
 	
+	if (!IsJumping && !IsWalking) {
+		currSprite = 0;
+	}
 	else if(IsJumping) {
 		currSprite = 5;
 		animationClock.restart();
 	}
-	else {
+	else if(IsWalking) {
 		if (animationClock.getElapsedTime().asMilliseconds() >= 100) {
 			currSprite++;
 			if (currSprite > 4) {
@@ -170,9 +188,28 @@ void Game::Character::changeSprite(int changeTo)
 			}
 			animationClock.restart();
 		}
-		}
+	}
+	else {
+		currSprite = 0;
+	}
 		
 		
 	sprite->setTextureRect(SpriteStates[currSprite]);
 
+}
+
+void Game::Character::hit(sf::RenderWindow& rw)
+{
+	currSprite = 6;
+	sprite->setTextureRect(SpriteStates[currSprite]);
+	sf::Font f;
+	f.loadFromFile("C://Windows//Fonts//Arial.ttf");
+	
+	sf::Text t("you lost", f, 40u);
+	t.setFillColor(sf::Color::White);
+	t.setPosition(this->position.x, this->position.y - 200);
+	rw.draw(*sprite);
+	rw.draw(t);
+	rw.display();
+	
 }
--- mario cplus/Character.hpp
+++ mario cplus/Character.hpp
@@ -8,25 +8,30 @@ using sf::Vector2f;
 namespace Game {
 	class Character : public AnimatedObject
 	{
-		//Represents the player's character in the game
+		///<summary>
+		///Class: Character
+		///
+		///The class that represents the player in the game.
+		///
+		///</summary>
 	protected:
 		
-		Vector2f VelocityBeforeJumping;
-		const Vector2f StartSpeed;
+		Vector2f VelocityBeforeJumping;//what the players velocity was before the jump function was executed
+		const Vector2f StartSpeed;//original speed when starting to move
 		
 
 	public:
-		bool       IsWalking;
-		bool       IsJumping;
+		bool IsWalking;
+		bool IsJumping;
 		Character(const Vector2f& startPos, const Vector2f& startSize);
 		void addSprites();
-		void move(float delta, sf::View& v);
-		void update(sf::View& v, sf::Keyboard::Key k = sf::Keyboard::Key::Unknown);
-		void jump();
+		void move(float delta, sf::View& v);//move character and view according to speed, whether player is jumping
+		void update(sf::View& v);//Update position of character on screen
+		void jump();//Initiate jumping
 		void DisplayInfo();
 		void Draw(sf::RenderTarget& target, const sf::RenderStates& states = sf::RenderStates::Default);
-		void changeSprite(int changeTo = 0);
-
+		void changeSprite(int changeTo = 0);//change what sprite is displayed
+		void hit(sf::RenderWindow& rw);//when hit by enemy
 
 
 
