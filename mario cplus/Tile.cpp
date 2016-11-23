254857cbb095b5f921f2e694bdf4274791399573
gammabray <brayj456@gmail.com>
2016-11-23 21:49:53 +0000

    Implemented more of tile procedures and functions. Added copy constructor.

--- mario cplus/Tile.cpp
+++ mario cplus/Tile.cpp
@@ -1,16 +1,40 @@
 #include "Tile.hpp"
 #include <SFML\Graphics.hpp>
+#include <iostream>
 
-Game::Tile::Tile(const Vector2f & startPos, const Vector2f & startSize, sf::Texture t)
-	: DisplayObject(startPos,startSize)
+Game::Tile::Tile(const sf::Vector2f & startPos, const sf::Vector2f & startSize,Game::TileID ID, sf::Texture &t) : 
+	position(startPos),size(startSize)
 {
+	this->texture = std::make_unique<sf::Texture>();
+	this->sprite = std::make_unique<sf::Sprite>();
 
+	std::string filepath;
+	this->ID = ID;
+	switch (this->ID) {
+	case 0:
+		break;
+	case 1:
+		filepath = "Images\\tile1.gif";
+		break;
+	}
+	if (!texture->loadFromFile(filepath)) {
+		std::cout << "Failed to load tile texture";
+	}
+	sprite->setTexture(*texture);
+	sprite->setPosition(position);
+}
+
+Game::Tile::Tile(const Tile & copy){
+	this->ID = copy.ID;
+	this->position = copy.position;
+	this->size = copy.size;
+	this->texture = std::make_unique<sf::Texture>(*copy.texture);
+	this->sprite = std::make_unique<sf::Sprite>(*copy.sprite);
 }
 
 Game::Tile::~Tile()
 {
+	texture.release();
+	sprite.release();
 }
 
-void Game::Tile::Draw(sf::RenderTarget & target, const sf::RenderStates & states)
-{
-}
--- mario cplus/Tile.hpp
+++ mario cplus/Tile.hpp
@@ -2,17 +2,30 @@
 #ifndef TILE_HPP
 #define TILE_HPP
 #include <SFML\Graphics.hpp>
-#include "DisplayObject.hpp"
+#include <memory>
 namespace Game {
-	class Tile : public DisplayObject
+	typedef int TileID;
+	class Tile 
+		///<summary>
+		///Class : Tile
+		///
+		///A class for each tile (Each bit of the level). Solely used by
+		///the Level class to draw multiple tiles with one call of draw. 
+		///</summary>
 	{
 	protected:
-		
+		TileID ID;
+		sf::Vector2f position;
+		sf::Vector2f size;
 
 	public:
-		Tile(const Vector2f& startPos, const Vector2f& startSize,sf::Texture t = sf::Texture());
+		std::unique_ptr<sf::Texture> texture;
+		std::unique_ptr<sf::Sprite> sprite;
+		sf::Vector2f& getPosition(){ return position; }
+		Tile(const sf::Vector2f& startPos, const sf::Vector2f& startSize,TileID ID,sf::Texture &t = sf::Texture());
+		Tile(const Tile& copy);
 		~Tile();
-		void Draw(sf::RenderTarget& target, const sf::RenderStates& states = sf::RenderStates::Default);
+	
 
 	};
 }
