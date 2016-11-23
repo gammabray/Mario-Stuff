d3fa9f39cf7d50e6f678b262cd7c48e4b0a0cc7a
gammabray <brayj456@gmail.com>
2016-11-23 22:00:50 +0000

    Added ostream operator overload to class instead of it being in a source file

--- mario cplus/AnimatedObject.cpp
+++ mario cplus/AnimatedObject.cpp
@@ -6,9 +6,15 @@ Game::AnimatedObject::AnimatedObject(const Vector2f & startPos, const Vector2f& 
 
 
 }
+std::ostream & Game::operator<<(std::ostream & o, sf::Vector2f v)
+{
+	o << "x: " << v.x << " y: " << v.y; return o;
+}
 
 Game::AnimatedObject::~AnimatedObject()
 {
 
 	
 }
+
+
--- mario cplus/AnimatedObject.hpp
+++ mario cplus/AnimatedObject.hpp
@@ -8,11 +8,17 @@
 #include <memory>
 
 using sf::Vector2f;
-//base class for all animated ojects in game.
+
+
 namespace Game {
 	class AnimatedObject : public Game::DisplayObject {
+		///<summary>
+		/// Class: AnimatedObject (Abstract)
+		///
+		///</summary>
 	protected:
-		std::vector<sf::IntRect> SpriteStates;
+		friend std::ostream& operator<<(std::ostream& o, sf::Vector2f v);//for displaying position of object
+		std::vector<sf::IntRect> SpriteStates;//container of different sprites for animation
 		enum class direction {
 			LEFT,
 			RIGHT,									   
@@ -20,18 +26,17 @@ namespace Game {
 			DOWN,
 			STATIONARY
 		};
-		direction travelling;
-		int       currSprite;
-		sf::Clock animationClock;
-		sf::Clock speedClock;		  
+		direction travelling; //where the object is facing
+		int       currSprite; //current sprite the object is using
+		sf::Clock animationClock; //clock to time walkign animation
+		sf::Clock speedClock; // clock to measure time since last frame to smoothly move the same amount every frame		  
 		Vector2f  acceleration;
 		Vector2f  deceleration;
 		Vector2f  velocity;
+		
 	public:
-		virtual void addSprites() = 0;
-		virtual void move(float delta, sf::View& v) = 0;
-		virtual void update(sf::View& v, sf::Keyboard::Key k = sf::Keyboard::Key::Unknown) = 0;
-		virtual void DisplayInfo() = 0;
+		virtual void addSprites() = 0;//add co-ordinate information for sprite areas
+		virtual void DisplayInfo() = 0;// show where sprite is
 
 
 		AnimatedObject(const Vector2f& startPos, const Vector2f& startSize, int noOfSprites);
