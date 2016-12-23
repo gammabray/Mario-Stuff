e912b16d46030c0741fdf93380e9d8f37c08ee35
gammabray <brayj456@gmail.com>
2016-12-23 15:48:14 +0000

    Changed coin to DisplayObject type. Reduced score value

---
+++ mario cplus/Coin.cpp
@@ -0,0 +1,16 @@
+#include "Coin.hpp"
+const sf::Vector2f Game::Coin::s_size(16, 16);
+Game::Coin::Coin(const sf::Vector2f& startPos) : DisplayObject(startPos,s_size)
+{
+	if (!texture->loadFromFile("Images\\Coin.png")) {
+		std::cout << "Failed to load texture for coin...";
+	}
+	sprite->setTexture(*texture);
+
+}
+
+void Game::Coin::Draw(sf::RenderTarget & target, const sf::RenderStates & states)
+{
+	target.draw(*sprite);
+}
+
--- mario cplus/Coin.hpp
+++ mario cplus/Coin.hpp
@@ -1,17 +1,19 @@
 #pragma once
 #ifndef COIN_HPP
 #define COIN_HPP
-#include "AnimatedObject.hpp"
+#include "DisplayObject.hpp"
+#include <iostream>
 namespace Game {
-	class Coin : public AnimatedObject {
+	class Coin : public DisplayObject {
 	public:
-		const static int ID = 3;
-		const static int scoreGiven = 100;
-		Coin();
-		~Coin();
+		const static int s_ID = 3;
+		const static int s_scoreGiven = 25;
+		const static sf::Vector2f s_size ;
+		Coin(const sf::Vector2f& startPos);
+		void Draw(sf::RenderTarget& target, const sf::RenderStates& states = sf::RenderStates::Default);
 	
-	protected:
-
+	
+	
 	};
 
 }
