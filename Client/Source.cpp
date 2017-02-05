99c40ad9b5be346d1709b45d4a695d98b09c08a6
gammabray <brayj456@gmail.com>
2017-02-05 21:11:25 +0000

    New file

--- Client/Source.cpp
+++
@@ -1,9 +0,0 @@
-#include "..\mario cplus\Engine.hpp"
-#include "..\mario cplus\Background.hpp"
-int main() {
-
-	Game::Background::s_addTextures();
-	Game::Engine engine(0);
-	engine.Start();
-	return 0;
-}
