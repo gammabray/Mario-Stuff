e67f2f9f99e700b9f895542d97705f79cccc77f7
gammabray <brayj456@gmail.com>
2017-03-15 14:52:17 +0000

    Using tiled + TinyXML 2 for levels now. Added barebones Level 2

---
+++ mario cplus/Images/Levels/1/Level 1 Terrain.tsx
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Level 1 Terrain" tilewidth="32" tileheight="32" tilecount="31">
+ <tile id="0">
+  <image width="32" height="32" source="block.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="bottomedgeblock.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="leftbotcornerblock.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="leftedgeblock.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="lefttopcornerblock.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="metalblock.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="middleblock.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="rightbotcornerblock.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="rightedgeblock.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="righttopcornerblock.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="topedgeblock.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="../../powerblock.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="../../Common/checkflag.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="../../Common/checkpole.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="../../Common/finishflag.png"/>
+ </tile>
+ <tile id="15">
+  <image width="24" height="24" source="../../Collectibles/Coin.png"/>
+ </tile>
+ <tile id="16">
+  <image width="20" height="20" source="../../Collectibles/bw_fireball.png"/>
+ </tile>
+ <tile id="17">
+  <image width="20" height="20" source="../../Collectibles/fireball.png"/>
+ </tile>
+ <tile id="18">
+  <image width="20" height="20" source="../../Collectibles/jump.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="innerblocktopleft.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="innerblocktopleft.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="innerblocktopright.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="innerblockbotleft.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="innerblockbotright.png"/>
+ </tile>
+ <tile id="24">
+  <image width="32" height="32" source="leftblock.png"/>
+ </tile>
+ <tile id="25">
+  <image width="32" height="32" source="rightblock.png"/>
+ </tile>
+ <tile id="26">
+  <image width="32" height="32" source="doubleedgeblock.png"/>
+ </tile>
+ <tile id="27">
+  <image width="32" height="32" source="bridge.png"/>
+ </tile>
+ <tile id="28">
+  <image width="32" height="32" source="rightbridgeconnect.png"/>
+ </tile>
+ <tile id="29">
+  <image width="32" height="32" source="leftbridgeconnect.png"/>
+ </tile>
+ <tile id="30">
+  <image width="32" height="32" source="rightblock.png"/>
+ </tile>
+</tileset>
