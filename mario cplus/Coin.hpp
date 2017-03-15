ff716bdff0aa88a4e02167ddbc7f9ebd345cefd5
gammabray <brayj456@gmail.com>
2017-03-15 14:53:10 +0000

    Changed ID to worked with tiled levels

--- mario cplus/Coin.hpp
+++ mario cplus/Coin.hpp
@@ -7,7 +7,7 @@ namespace Game {
 	
 	class Coin : public DisplayObject {
 	public:
-		const static char s_ID = '3';
+		const static int s_ID = 16;
 		const static int s_scoreGiven = 25;
 		const static sf::Vector2f s_size;
 		Coin(const sf::Vector2f& startPos);
