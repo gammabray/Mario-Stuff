bb41ecf9c2ae9ad739bcdd1d18da0f019db81d60
gammabray <brayj456@gmail.com>
2017-03-15 14:58:15 +0000

    Main returns now

--- mario cplus/Source.cpp
+++ mario cplus/Source.cpp
@@ -1,6 +1,7 @@
 #include "Engine.hpp"
+#include "TinyXML\tinyxml2.h"
 #include <sstream>
-
+namespace XML = tinyxml2;
 
 int main(int argc, char** argv) {
 	std::cout << argc << std::endl;
@@ -14,7 +15,8 @@ int main(int argc, char** argv) {
 	Game::Background::s_addTextures();
 	Game::Engine Engine(startLevel);
 	Engine.Start();
-
+	return EXIT_SUCCESS;
+	
 
 
 }
